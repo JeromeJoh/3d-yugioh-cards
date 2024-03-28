a7563225dfa76c648e1efe44d17792509ee5cce1
JeromeJoh <jerome.yzhao@foxmail.com>
2024-03-28 17:05:44 +0800

    feat: add scroll view to vol2 page

--- data.js
+++ data.js
@@ -14,10 +14,22 @@ export const vol2 = {
   number: 2,
   theme: "Plant Princess",
   cards: [
-    "Tytannial, Princess of Camellias",
-    "Talaya, Princess of Cherry Blossoms",
-    "Malina, Princess of Sunflowers",
-    "Chirubimé,Princess of Autumn Leaves",
+    {
+      name: "Tytannial, Princess of Camellias",
+      effect: "when a card or effect is activated that target a card(s) on the field(Quick Effect): You can Tribute 1 face-up Plant monster; negate the activation, and if you do, destroy it."
+    },
+    {
+      name: "Talaya, Princess of Cherry Blossoms",
+      effect: "Gain 100 ATK for each Plant monster you control. Other monsters on the field cannot be destroyed by card effects."
+    },
+    {
+      name: "Malina, Princess of Sunflowers",
+      effect: "If exactly 1 other Plant monster you control(an no other cards) is destroyed by battle or card effect and sent to the GY: You can target 1 card your opponent controls; destroy that target. This card must be face-up on the field to activate and to resolve this effect."
+    },
+    {
+      name: "Chirubimé,Princess of Autumn Leaves",
+      effect: `Your opponent cannot target face-up Plant monster for attacks, excep this one. If this card in your possession is sent to your GY by your opponent's card: You can Special Summon 1 Plant monster from your Deck, except "Chirubimé,Princess of Autumn Leaves".`
+    }
   ]
 }
 
--- vol2/index.js
+++ vol2/index.js
@@ -11,3 +11,100 @@ requestAnimationFrame(raf);
 
 
 // initialize page
+import { vol2 } from '/data.js';
+
+const init = () => {
+  render(vol2);
+};
+
+function render(vol) {
+  const cardTitles = document.querySelectorAll('.content h1');
+  const cardEffects = document.querySelectorAll('.content p');
+
+  for (let i = 0; i < 4; i++) {
+    cardTitles[i].textContent = vol.cards[i].name;
+    cardEffects[i].textContent = vol.cards[i].effect;
+  }
+}
+
+init();
+
+
+
+
+// scroll animation
+gsap.registerPlugin(ScrollTrigger);
+
+const slider = document.querySelector('.slider');
+const sections = gsap.utils.toArray('.slider section');
+
+const scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: slider,
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () => `+=${slider.offsetWidth}`
+  }
+});
+
+sections.forEach((section, i) => {
+  gsap.from(section.querySelector('h1'), {
+    yPercent: 135,
+    scrollTrigger: {
+      containerAnimation: scrollTween,
+      trigger: section.querySelector('h1'),
+      start: 'left center',
+      toggleActions: 'play none none reverse'
+    }
+  });
+
+  const pragraph = new SplitType(section.querySelectorAll('p'));
+  console.log(pragraph.words);
+
+  gsap.from(pragraph.words, {
+    y: 40,
+    opacity: 0,
+    skewX: 30,
+    stagger: 0.03,
+    scrollTrigger: {
+      containerAnimation: scrollTween,
+      trigger: section.querySelector('h1'),
+      start: 'left center',
+      toggleActions: 'play none none reverse'
+    }
+  });
+
+
+  gsap.from(section.querySelector('p'), {
+    yPercent: 50,
+    opacity: 0,
+    scrollTrigger: {
+      containerAnimation: scrollTween,
+      trigger: section.querySelector('p'),
+      start: 'left center',
+      toggleActions: 'play none none reverse'
+    }
+  }, '<');
+
+  gsap.from(section.querySelector('.card'), {
+    y: 50,
+    opacity: 0,
+    scrollTrigger: {
+      containerAnimation: scrollTween,
+      trigger: section,
+      start: 'left center',
+      toggleActions: 'play none none reverse'
+    }
+  });
+
+  gsap.to('.inner', {
+    scaleX: 1,
+    scrollTrigger: {
+      scrub: .25
+    }
+  })
+
+});
